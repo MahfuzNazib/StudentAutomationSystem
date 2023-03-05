5f32732d40e7a6f24d8cc33a748795152662d91e
MahfuzNazib <nazibmahfuz60@gmail.com>
2023-03-05 17:46:39 +0600

    Save Student Record on DB via QueryString

---
+++ StudentAutomationSystem/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using StudentAutomationSystem.Models;
+
+namespace StudentAutomationSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        string connectionString;
+
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Create New Student
+        public string CreateStudent(Student student)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            
+            connection.Open();
+            
+            string sql = "INSERT INTO Students (StudentCode, StudentName,StudentPhone,StudentEmail,StudentAddress) " +
+                "VALUES('"+student.StudentCode+"', '"+student.StudentName+"', '"+student.StudentPhone+"', '"+student.StudentEmail+"', '"+student.StudentAddress+"')";
+            
+            SqlCommand cmd = new SqlCommand(sql, connection); 
+
+            if(cmd.ExecuteNonQuery() > 0)
+            {
+                connection.Close();
+                return "Success";
+            }
+            connection.Close();
+            return "Failed";
+        }
+
+        public ActionResult AllStudents()
+        {
+            return View();
+        }
+
+        public List<Student> GetStudents()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string sql = "SELECT * FROM Students";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+        }
+    }
+}
---
+++ StudentAutomationSystem/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentAutomationSystem.Models
+{
+    public class Student
+    {
+        public string StudentCode { get; set; }
+        public string StudentName { get; set;}
+        public string StudentPhone { get; set;}
+        public string StudentEmail { get; set;}
+        public string StudentAddress { get; set;}
+
+    }
+}
