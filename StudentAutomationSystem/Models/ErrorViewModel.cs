a626c77c514412892767b28d91cfdf2016c879d4
MahfuzNazib <nazibmahfuz60@gmail.com>
2023-03-05 14:44:19 +0600

    First Commit

---
+++ StudentAutomationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentAutomationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
